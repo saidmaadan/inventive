22b99dc2a1d69c445a6807897cbb280308c33def
Said Maadan <saidmaadan@users.noreply.github.com>
2016-08-03 14:14:22 -0500

    Created landing page and defined add product validation rules

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,14 +8,29 @@ class ProductsController < ApplicationController
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  def delete
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :delivery_time, :revisions, :requirements)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ActiveRecord::Base
   validates :title, presence: true, length: {minimum:8}
-  validates :description, presence: true, length: {minimum:80}
+  validates :description, presence: true, length: {minimum:30}
   validates :price, presence: true
   validates :delivery_time, presence: false
   validates :revisions, presence: false
-  validates :requirements, presence: true, length: {minimum:80}
+  validates :requirements, presence: true
 end
